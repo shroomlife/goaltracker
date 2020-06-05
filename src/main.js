d094add7be8df6dc14ef919967a116ef865e6bb5
shroomlife <robin@shroomlife.de>
2020-06-05 19:25:05 +0200

    added bootstrap

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
